00ba033ac0cb7e44df4254fc4ca86d2e92d05668
Gulkovva19 <gulkova.sofya@yandex.ru>
2021-12-29 14:36:33 +0300

    homework

---
+++ index.js
@@ -0,0 +1,31 @@
+function calcSum() {
+    let a = prompt("Первое число");
+    let b = prompt("Второе число");
+    let sum = Number(a) + Number(b);
+
+    alert(sum);
+}
+
+function calcSub() {
+    let a = prompt("Первое число");
+    let b = prompt("Второе число");
+    let sub = Number(a) - Number(b);
+
+    alert(sub);
+}
+
+function calcMulti() {
+    let a = prompt("Первое число");
+    let b = prompt("Второе число");
+    let multi = Number(a) * Number(b);
+
+    alert(multi);
+}
+
+function calcDiv() {
+    let a = prompt("Первое число");
+    let b = prompt("Второе число");
+    let div = Number(a) / Number(b);
+
+    alert(div);
+}
